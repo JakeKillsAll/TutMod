c64776949e194bf5ce6368e704a7b85e5f2ba554
JakeKillsAll <dabossjake1@gmail.com>
2017-05-15 23:12:56 -0400

    Maybe the mod will show???

--- src/main/java/com/LethalLeonard/tutmod/TutMod.java
+++ src/main/java/com/LethalLeonard/tutmod/TutMod.java
@@ -1,7 +1,13 @@
 package com.LethalLeonard.tutmod;
 
+import net.minecraftforge.fml.common.Mod;
+
 /**
  * Created by Jake Leonard on 5/15/2017.
  */
+@Mod(modid = "tutmod", name = "TutMod", version = "0.1a")
 public class TutMod {
+
+
+
 }
