7e16b190124cc3f343cdcbb7608bf05dc072b676
JakeKillsAll <dabossjake1@gmail.com>
2017-05-21 23:07:27 -0400

    Created a logging method as well as making the configuration work

--- src/main/java/com/LethalLeonard/tutmod/handler/ConfigurationHandler.java
+++ src/main/java/com/LethalLeonard/tutmod/handler/ConfigurationHandler.java
@@ -1,43 +1,33 @@
 package com.LethalLeonard.tutmod.handler;
 
-import com.LethalLeonard.tutmod.reference.Reference;
+import com.LethalLeonard.tutmod.utility.LogHelper;
 import net.minecraftforge.common.config.Configuration;
-import net.minecraftforge.fml.client.event.ConfigChangedEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.io.File;
 
 public class ConfigurationHandler
 {
-    public static Configuration configuration;
-    public static boolean testValue = false;
-
     public static void init(File configFile)
     {
-        // Create the configuration object from the given configuration file
-        if (configuration == null)
+        Configuration configuration = new Configuration(configFile);
+
+        try
         {
-            configuration = new Configuration(configFile);
-            loadConfiguration();
+            configuration.load();
+
+            boolean configValue = configuration.getBoolean("Sample Value", "General", true,
+                    "This is a test config value");
         }
-    }
-
-    private static void loadConfiguration()
-    {
-        testValue = configuration.getBoolean("configValue", Configuration.CATEGORY_GENERAL, false, "This is an example configuration value");
-
-        if (configuration.hasChanged())
+        catch(Exception e)
         {
-            configuration.save();
+            LogHelper.error(e);
         }
-    }
-
-    @SubscribeEvent
-    public void onConfigurationChangedEvent(ConfigChangedEvent.OnConfigChangedEvent event)
-    {
-        if (event.getModID().equalsIgnoreCase(Reference.modid))
+        finally
         {
-            loadConfiguration();
+            if(configuration.hasChanged())
+            {
+                configuration.save();
+            }
         }
     }
 }
